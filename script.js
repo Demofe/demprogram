b0ed19fe4b0d7046d81bc8306e23d6e5553d9daf
Demofe <60311716+Demofe@users.noreply.github.com>
2020-01-26 23:28:51 +0300

    1.test

---
+++ script.js
@@ -0,0 +1,10 @@
+  $('document').ready(function(){
+  jQuery('body').append('<a href="https://www.youtube.com/channel/UCdAJAXK656BCPXZ8R0Nqu5A?view_as=subscriber">ето мой канал</a>');	
+  });
+  $('document').ready(function(){
+	  
+  jQuery('button').on('click',function(){
+	 alert('Спасибо за то ,что ты скачал этот фаил');  
+  });
+  
+  });
